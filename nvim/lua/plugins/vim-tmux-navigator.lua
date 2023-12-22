9c6f6461758808e3982d9d839250cd17b65220bd
AndrewNgo-ini <hieungo1898@gmail.com>
2023-12-22 23:18:03 +0700

    update

--- nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,19 +1,4 @@
 return {
-  "alexghergh/nvim-tmux-navigation",
-  event = "VeryLazy",
-  config = function()
-    local nvim_tmux_nav = require("nvim-tmux-navigation")
-    nvim_tmux_nav.setup({
-      disable_when_zoomed = true,
-      -- defaults to false
-      keybindings = {
-        left = "<C-h>",
-        down = "<C-j>",
-        up = "<C-k>",
-        right = "<C-l>",
-        last_active = "<C-\\>",
-        next = "<C-Space>",
-      },
-    })
-  end,
+  "christoomey/vim-tmux-navigator",
+  lazy = false,
 }
