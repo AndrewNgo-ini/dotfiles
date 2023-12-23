1e0258d09a3373f4d68cfdfb8e3ce4fe08dc1879
AndrewNgo-ini <hieungo1898@gmail.com>
2023-12-23 13:49:00 +0700

    update

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -5,9 +5,9 @@ return {
       config = function()
         local config = require("nvim-treesitter.configs")
         config.setup({
-          ensure_installed = {"lua", "javascript"},
+          ensure_installed = {"lua", "python", "rust"},
           highlight = { enable = true },
-          indent = { enable = true },  
+          indent = { enable = true },
         })
       end
     }
--- nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,4 +1,12 @@
 return {
   "christoomey/vim-tmux-navigator",
   lazy = false,
+  config = function()
+    vim.keymap.set("n", "<C-h>", ":TmuxNavigateLeft<CR>", { silent = true })
+    vim.keymap.set("n", "<C-j>", ":TmuxNavigateDown<CR>", { silent = true })
+    vim.keymap.set("n", "<C-k>", ":TmuxNavigateUp<CR>", { silent = true })
+    vim.keymap.set("n", "<C-l>", ":TmuxNavigateRight<CR>", { silent = true })
+    --vim.keymap.set("n", "<C-\\>", "<Cmd>NvimTmuxNavigateLastActive<CR>", { silent = true })
+    --vim.keymap.set("n", "<C-Space>", "<Cmd>NvimTmuxNavigateNavigateNext<CR>", { silent = true })
+  end
 }
