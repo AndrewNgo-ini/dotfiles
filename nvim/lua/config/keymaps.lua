650b729ac48a41592aec95c1307760ca6b2e6487
AndrewNgo-ini <hieungo1898@gmail.com>
2023-12-25 23:33:22 +0700

    update

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -7,6 +7,12 @@ keymap.set('n', '<C-U>', '<C-U>zz', opts)
 keymap.set('n', '<C-F>', '<C-F>zz', opts)
 keymap.set('n', '<C-B>', '<C-B>zz', opts)
 
+-- Learn from primagen :d 
+keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+keymap.set('n', 'n', "nzzzv")
+keymap.set('N', 'N', "Nzzzv")
+
 -- Indenting
 keymap.set("v", "<", "<gv")
 keymap.set("v", ">", ">gv")
