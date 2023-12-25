ca7cd69017b6c27451ee883b0ed244a556a5dda6
hieu <hieu.ngo3@mservice.com.vn>
2023-12-25 21:28:38 +0700

    update

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -19,8 +19,8 @@ return {
     lazy = false,
     config = function()
       local lspconfig = require("lspconfig")
-      lspconfig.tsserver.setup({})
       lspconfig.lua_ls.setup({})
+      lspconfig.pyright.setup({})
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
