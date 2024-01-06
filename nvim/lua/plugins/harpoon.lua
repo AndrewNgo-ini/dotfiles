d6416799e9ff7f4c97a3b2888d0794dcfbbfbddb
AndrewNgo-ini <hieungo1898@gmail.com>
2024-01-06 21:15:48 +0700

    update

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -6,9 +6,9 @@ return {
   },
   config = function ()
     require("harpoon").setup({})
-    vim.keymap.set('n', '<leader>hm', "<cmd>lua require('harpoon.mark').add_file()<cr>", {})
-    vim.keymap.set('n', '<leader>hn', "<cmd>lua require('harpoon.ui').nav_next()<cr>", {})
-    vim.keymap.set('n', '<leader>hp', "<cmd>lua require('harpoon.ui').nav_prev()<cr>", {})
+    vim.keymap.set('n', '<leader>ha', "<cmd>lua require('harpoon.mark').add_file()<cr>", {})
+    vim.keymap.set('n', '<M-e>', "<cmd>lua require('harpoon.ui').nav_next()<cr>", {})
+    vim.keymap.set('n', '<M-q>', "<cmd>lua require('harpoon.ui').nav_prev()<cr>", {})
     vim.keymap.set('n', '<leader>hs', "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", {})
   end,
 }
