7fc8d72712498e5147b59218bb60bacf5635f21c
AndrewNgo-ini <hieungo1898@gmail.com>
2024-01-24 15:30:12 +0700

    update

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -22,7 +22,6 @@ return {
         },
       },
     })
-    vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
-    vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+    vim.keymap.set("n", "<leader>P", ":Neotree filesystem reveal left toggle=true<CR>", { noremap = true, silent = true })
   end,
 }
